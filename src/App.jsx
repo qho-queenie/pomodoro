2f5633a302339f4da641174c050b873555f7ce06
Queenie Ho <qho.queenieho@gmail.com>
2021-11-12 15:31:05 -0800

    git amend didnt work, remove ignore rule

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import ClockFace from './components/ClockFace';
 import SettingsModal from './components/SettingsModal';
 import './App.css';
 
-/**
- * Generates the App div, which contains the entire app
- * @return {ClockFace} The App
- */
 function App() {
   return (
     <div className="App">
